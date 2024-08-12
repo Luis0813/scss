919fc2cb50477914de8bbe1ce4dab6f43af3bd4e
Ferando Rivero <lr4136196@gmail.com>
2024-08-11 21:01:14 -0400

    primer-scss

---
+++ index.js
@@ -0,0 +1,16 @@
+let arreglo =[1,2,3]
+let arreglo3 = []
+let persona = {
+    nombre:'fernando', 
+    edad: '15',
+    id:Date.now()
+}
+/* arreglo.push(persona) */
+
+arreglo.push(5,4)
+console.log (arreglo)
+        arreglo.map(arreglo2 => {
+            arreglo2 = arreglo 
+            console.log(arreglo2)
+        });
+
